ee3f3e58927be0a7828007b31da13f1f8fd0c125
Vampire Squad <vampiresquad.org@gmail.com>
2026-01-21 15:51:05 +0600

    Update questions.js

--- assets/data/questions.js
+++ assets/data/questions.js
@@ -1,1 +1,10 @@
-
+// questions.js
+export const questions = [
+  {
+    id: 1,
+    category: "science",
+    q: "পানির রাসায়নিক সংকেত কী?",
+    o: ["CO2", "H2O", "O2", "NaCl"],
+    a: 1
+  }
+];
