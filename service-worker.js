8aa9032e00344c691352e302f3927b1b485b633a
Vampire Squad <vampiresquad.org@gmail.com>
2026-01-22 00:21:06 +0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -6,39 +6,35 @@
 const CACHE_NAME = 'quiz-master-v1';
 
 const ASSETS_TO_CACHE = [
-  '/',
-  '/index.html',
-  '/manifest.json',
+  './',
+  './index.html',
+  './manifest.json',
 
   // CSS
-  '/assets/css/base.css',
-  '/assets/css/layout.css',
-  '/assets/css/components.css',
-  '/assets/css/animations.css',
-  '/assets/css/theme.css',
+  './assets/css/base.css',
+  './assets/css/layout.css',
+  './assets/css/components.css',
+  './assets/css/animations.css',
+  './assets/css/theme.css',
 
   // JS
-  '/assets/js/main.js',
-  '/assets/js/engine.js',
-  '/assets/js/ui.js',
-  '/assets/js/timer.js',
-  '/assets/js/audio.js',
-  '/assets/js/story.js',
-  '/assets/js/personality.js',
-  '/assets/js/adaptive.js',
+  './assets/js/main.js',
+  './assets/js/engine.js',
+  './assets/js/ui.js',
+  './assets/js/timer.js',
+  './assets/js/audio.js',
+  './assets/js/story.js',
+  './assets/js/personality.js',
+  './assets/js/adaptive.js',
 
   // Data
-  '/assets/data/questions.js',
-
-  // Fonts (Google fonts cache hint)
-  'https://fonts.googleapis.com/css2?family=Hind+Siliguri:wght@400;600;700&display=swap'
+  './assets/data/questions.js'
 ];
 
 /* Install */
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(ASSETS_TO_CACHE))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS_TO_CACHE))
   );
   self.skipWaiting();
 });
@@ -48,8 +44,8 @@ self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys =>
       Promise.all(
-        keys.filter(k => k !== CACHE_NAME)
-            .map(k => caches.delete(k))
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
       )
     )
   );
@@ -59,8 +55,8 @@ self.addEventListener('activate', event => {
 /* Fetch */
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(res => res || fetch(event.request))
-      .catch(() => caches.match('/index.html'))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    }).catch(() => caches.match('./index.html'))
   );
 });
