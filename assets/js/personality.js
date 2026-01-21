1692fa11d7c555cb299308051b2399d0f27ca951
Vampire Squad <vampiresquad.org@gmail.com>
2026-01-21 16:09:45 +0600

    Update personality.js

--- assets/js/personality.js
+++ assets/js/personality.js
@@ -1,1 +1,7 @@
-
+export function analyze(state) {
+  if(state.timeAvg < 6 && state.wrong < 2)
+    return "Lightning Thinker ⚡";
+  if(state.lifeline)
+    return "Strategic Survivor 🧠";
+  return "Calm Observer 🌊";
+}
