e6615ed8923c29198b50f7554f76d7103fc577a1
Vampire Squad <vampiresquad.org@gmail.com>
2026-01-21 15:50:36 +0600

    Update engine.js

--- assets/js/engine.js
+++ assets/js/engine.js
@@ -1,1 +1,14 @@
-
+// engine.js
+export const GameState = {
+  index: 0,
+  score: 0,
+  timeHistory: [],
+  wrong: 0,
+  streak: 0,
+  lifeline: false,
+  mode: {
+    silent: false,
+    story: true,
+    adaptive: true
+  }
+};
